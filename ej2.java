00e505968f999b3919ac30149730678dc88cadb9
unknown <jorge.prieto.loimil@gmail.com>
2020-06-06 14:02:28 +0200

    Suma

---
+++ ej2.java
@@ -0,0 +1,12 @@
+public class Suma {
+
+    public static void main(String[] args) {
+
+        int num1 =20;
+        int num2 =3;
+        int resultado = num1 + num2;
+
+        System.out.println("El resultado es:"+resultado);
+
+    }
+}
